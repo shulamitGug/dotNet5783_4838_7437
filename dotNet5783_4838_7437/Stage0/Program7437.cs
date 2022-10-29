f146aa32076961730ba2e20934c938dd22e50aff
shulamitGug <shgug24@gmail.com>
2022-10-29 22:49:11 +0300

    Initinal code+welcome function

---
+++ dotNet5783_4838_7437/Stage0/Program7437.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace System
+{
+   partial class Program
+    {
+       public static void main()
+        {
+            welcome7437();
+            welcome4838();
+        }
+        static partial void welcome4838();
+
+        private static void welcome7437()
+        {
+            Console.Write("Enter your name: ");
+            string myName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first application ", myName);
+            Console.ReadKey();
+        }
+         
+    }
+    
+}
